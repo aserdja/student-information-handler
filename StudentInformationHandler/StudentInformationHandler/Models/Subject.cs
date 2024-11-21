4e6cebe33fc68ed4cf3968f53783151cda3073ee
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-21 19:10:42 +0200

    Create Subject class. Add properties

---
+++ StudentInformationHandler/StudentInformationHandler/Models/Subject.cs
@@ -0,0 +1,28 @@
+﻿namespace StudentInformationHandler.Models
+{
+	public class Subject
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public int StudentId { get; set; }
+		public int Grade
+		{
+			get
+			{
+				return Grade;
+			}
+			set
+			{
+				if (value > 0 && value <= 100)
+				{
+					Grade = value;
+				}
+				else
+				{
+					throw new ArgumentOutOfRangeException();
+				}
+			}
+		}
+		public DateOnly Date { get; set; }
+	}
+}
