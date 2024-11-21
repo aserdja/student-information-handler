74dbde4149cda0efc96cc8c894e3fbed2e416b24
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-21 20:39:50 +0200

    Change Id-type from int to Guid

--- StudentInformationHandler/StudentInformationHandler/Models/Student.cs
+++ StudentInformationHandler/StudentInformationHandler/Models/Student.cs
@@ -2,7 +2,7 @@
 {
 	public class Student
 	{
-		public int Id { get; set; }
+		public Guid Id { get; set; }
 		public string FirstName { get; set; } = string.Empty;
 		public string LastName { get; set; } = string.Empty;
 		public int Age { get; set; }
--- StudentInformationHandler/StudentInformationHandler/Models/Subject.cs
+++ StudentInformationHandler/StudentInformationHandler/Models/Subject.cs
@@ -2,7 +2,7 @@
 {
 	public class Subject
 	{
-		public int Id { get; set; }
+		public Guid Id { get; set; }
 		public string Name { get; set; } = string.Empty;
 		public int StudentId { get; set; }
 		public int Grade
