3355fe7979bd456246aa7c297121f557749a03d0
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-21 21:44:17 +0200

    Add test-logic and console output

--- StudentInformationHandler/StudentInformationHandler/Program.cs
+++ StudentInformationHandler/StudentInformationHandler/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using StudentInformationHandler.Models;
+
+List<Student> students = Student.Fill().ToList();
+
+
+foreach (Student student in students)
+{
+	student.SetSubjects(Subject.Fill(student.Id));
+	student.AverageGrade = student.CalculateAverageGrade();
+	student.SetGrand();
+}
+
+
+foreach (Student student in students)
+{
+	Console.WriteLine(new string('-', 15));
+
+	Console.WriteLine($"Id: {student.Id} \nFirst name: {student.FirstName} \nLast name: {student.LastName} \nAge: {student.Age} \nAverage grade: {student.AverageGrade} " +
+		$"\nGrant: {student.Grant}");
+	Console.WriteLine($"Subjects:");
+	foreach (var subject in student.Subjects)
+	{
+		Console.WriteLine($"Id: {subject.Id}; Name: {subject.Name}; Grade: {subject.Grade}; Date: {subject.Date}");
+	}
+
+	Console.WriteLine(new string('-', 15));
+}
