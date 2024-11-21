8cb946e78c80ff66b1061e6331e18c6b0a173456
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-21 19:17:34 +0200

    Create Student class. Add properties

---
+++ StudentInformationHandler/StudentInformationHandler/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentInformationHandler.Models
+{
+	public class Student
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; } = string.Empty;
+		public string LastName { get; set; } = string.Empty;
+		public int Age { get; set; }
+		public List<Subject> Subjects { get; set; } = new();
+		public float AverageGrade { get; set; }
+		public Grant Grant { get; set; }
+	}
+}
